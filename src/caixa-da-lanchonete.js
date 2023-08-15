07da14d5e8526bcbd9fe4806a02326de825df82b
leon <de.nizard@hotmail.com>
2023-08-14 21:02:24 -0300

    Refatorado objeto 'cardapio', removendo propriedades não utilizadas

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,10 +1,10 @@
 class CaixaDaLanchonete {
   cardapio = {
     cafe: { descricao: "Café", valor: 3.0 },
-    chantily: { descricao: "Chantily (extra do Café)", valor: 1.5, itemPrincipal: 'cafe' },
+    chantily: { descricao: "Chantily (extra do Café)", valor: 1.5 },
     suco: { descricao: "Suco Natural", valor: 6.2 },
     sanduiche: { descricao: "Sanduíche", valor: 6.5 },
-    queijo: { descricao: "Queijo (extra do Sanduíche)", valor: 2.0, itemPrincipal: 'sanduiche' },
+    queijo: { descricao: "Queijo (extra do Sanduíche)", valor: 2.0 },
     salgado: { descricao: "Salgado", valor: 7.25 },
     combo1: { descricao: "1 Suco e 1 Sanduíche", valor: 9.5 },
     combo2: { descricao: "1 Café e 1 Sanduíche", valor: 7.5 },
@@ -81,8 +81,8 @@ class CaixaDaLanchonete {
     // const totalFormatado = moedaReal.format(total)
     // return totalFormatado
 
-    const totalFormatado = total.toFixed(2).replace('.',',');
-    return `R$ ${totalFormatado}`;
+    const totalFormatado = total.toFixed(2).replace('.',',')
+    return `R$ ${totalFormatado}`
   }
 }
 
